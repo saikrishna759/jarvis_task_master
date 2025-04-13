52566ee582a36d4ea125a8616b0af09b3900f130
saikrishna759 <sai.sanniboina@gmail.com>
2025-04-13 01:27:12 -0500

    merged multiturn

--- client/src/components/VoiceAssistant.js
+++ client/src/components/VoiceAssistant.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import useSpeech from "../hooks/useSpeech";
 import { interpretVoiceCommand } from "../utils/api";
 
